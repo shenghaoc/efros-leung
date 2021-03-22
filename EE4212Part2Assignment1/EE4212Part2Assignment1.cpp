eb8e805aa2a0b9d8e5c821d9ff9fed8eb1cd5618
Chen Shenghao <chen.shenghao@u.nus.edu>
2021-03-23 03:12:57 +0800

    Add PixelList

--- EE4212Part2Assignment1/EE4212Part2Assignment1.cpp
+++ EE4212Part2Assignment1/EE4212Part2Assignment1.cpp
@@ -1,25 +1,48 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 using namespace cv;
 using namespace std;
+
+int scale = 3;
+int threshold_value = 0;
+int threshold_type = THRESH_BINARY;
+int const max_binary_value = 255;
+
 int main()
 {
-    Mat SampleImage;
-    SampleImage = imread("texture1.jpg", IMREAD_COLOR); // Read the file
-    if (SampleImage.empty()) // Check for invalid input
-    {
-        cout << "Could not open or find the sample image" << std::endl;
-        return -1;
-    }
-    // Let synthesized image be 9 times the area of the sample image
-    Mat Image = Mat::zeros(3 * SampleImage.rows, 3 * SampleImage.cols, CV_8UC3);
-    // Copy sample image to upper left corner of synthesized image
-    SampleImage.copyTo(Image(Rect(0, 0, SampleImage.cols, SampleImage.rows)));
-    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Display window", Image); // Show our image inside it.
-    waitKey(0); // Wait for a keystroke in the window
-    imwrite("result1.jpg", Image); // Save synthesized image
-    return 0;
+	Mat SampleImage;
+	SampleImage = imread("texture1.jpg", IMREAD_COLOR); // Read the file
+	if (SampleImage.empty()) // Check for invalid input
+	{
+		cout << "Could not open or find the sample image" << std::endl;
+		return -1;
+	}
+
+	// Let synthesized image be scale^2 times the area of the sample image
+	Mat Image = Mat::zeros(scale * SampleImage.rows, scale * SampleImage.cols, CV_8UC3);
+	// Copy sample image to upper left corner of synthesized image
+	SampleImage.copyTo(Image(Rect(0, 0, SampleImage.cols, SampleImage.rows)));
+
+	Mat UnfilledNeighbors;
+	Mat element = getStructuringElement(MORPH_RECT, Size(3, 3));
+	Mat mask;
+	// Binary mask ensures dilation ignores regions within already filled pixels
+	threshold(Image, mask, threshold_value, max_binary_value, threshold_type);
+	dilate(mask, UnfilledNeighbors, element);
+	// Enlarged filled pixel region - filled pixel region = border formed by 
+	// pixels along border of filled pixel region
+	UnfilledNeighbors -= mask;
+	// Convert to grayscale for findNonZero() to work
+	cvtColor(UnfilledNeighbors, UnfilledNeighbors, COLOR_RGB2GRAY, 0);
+	Mat PixelList;
+	findNonZero(UnfilledNeighbors, PixelList);
+
+	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+	imshow("Display window", UnfilledNeighbors); // Show our image inside it.
+	waitKey(0); // Wait for a keystroke in the window
+	imwrite("result1.jpg", UnfilledNeighbors); // Save synthesized image
+	return 0;
 }
